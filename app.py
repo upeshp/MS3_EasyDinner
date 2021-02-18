87f889adb02acd999921a553a176ce90ee0f0206
upeshp <upesh.patel@hotmail.com>
2021-02-18 20:08:14 +0000

    Feat: added the register.html page for signup functionality, added code for register app in app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -20,8 +21,13 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_recipes")
 def get_recipes():
-    recipes = mongo.db.recipes.find() # gets recipes from database
+    recipes = mongo.db.recipes.find()  # gets recipes from database
     return render_template("recipes.html", recipes=recipes)
+
+
+@app.route("/register", methods=["GET", "POST"])  
+def register():
+    return render_template("register.html")  # registration
 
 
 if __name__ == "__main__":
