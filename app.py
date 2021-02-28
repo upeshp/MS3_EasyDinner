cb2fd101ebc436a5c5dedd1520a7467df13c0c81
upeshp <upesh.patel@hotmail.com>
2021-02-28 17:12:27 +0000

    Feat: adding 500.html error page, adding code in app.py to render this page on error

--- app.py
+++ app.py
@@ -177,6 +177,11 @@ def page_not_found(error):
     return render_template('404.html'), 404
 
 
+@app.errorhandler(500)  # 500 ERROR
+def something_wrong(error):
+    return render_template('500.html'), 500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
